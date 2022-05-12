2d2c80e2b8329959b30381cafe7de7ce70dee9f0
Caio Gomes <caiogomes34@gmail.com>
2022-05-12 19:34:36 +0100

    implement the transformation pipelines

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { computeTransformationMatrix } from './transform';
+import { getTransformationMatrix } from './transform';
 
 test('Compute Identity matrix test',() => {
   const identityMatrix = '[[1,0,0],[0,1,0],[0,0,1]]'
   const points = [{x: 1, y: 2}, {x: 2, y: 1}, {x: 1, y: 1}, {x: 2, y:2}]
-  const transformationMatrix = computeTransformationMatrix(points, points)
+  const transformationMatrix = getTransformationMatrix(points, points)
   const relatedMatrix = transformationMatrix.map(row => row.map(cell => Math.round(cell)))
   expect(JSON.stringify(relatedMatrix)).toBe(identityMatrix)
 })
--- src/transform.js
+++ src/transform.js
@@ -6,14 +6,14 @@ const math = create(all, config)
 // @typedef {x: number, y: number} point
 
 const getBaseMatrix = (points) => {
-  return  [
+  return [
     [points[0].x, points[1].x, points[2].x],
     [points[0].y, points[1].y, points[2].y],
     [1, 1, 1]
   ]
 }
 
-const computeMainVector = (points) => {
+const getMainVector = (points) => {
   const baseMatrix = getBaseMatrix(points)
   const baseVec = [points[3].x, points[3].y, 1]
   const ansVec = math.usolve(baseMatrix, baseVec)
@@ -26,20 +26,85 @@ const computeMainVector = (points) => {
  * @param {point[]} outputPoints 
  * @returns 
  */
-const computeTransformationMatrix = (inputPoints, outputPoints) => {
+const getTransformationMatrix = (inputPoints, outputPoints) => {
   if (inputPoints.length !== outputPoints.length || inputPoints.length === 0) {
     throw Error("Input and output points doesn't match in size.")
   }
 
-  const inputMainVector = computeMainVector(inputPoints)
-  const outputMainVector = computeMainVector(outputPoints)
-  const p1 = getBaseMatrix(inputPoints).map(row => row.map((cell, idx) => cell*inputMainVector[idx]))
-  const p2 = getBaseMatrix(outputPoints).map(row => row.map((cell, idx) => cell*outputMainVector[idx]))
+  const inputMainVector = getMainVector(inputPoints)
+  const outputMainVector = getMainVector(outputPoints)
+  const p1 = getBaseMatrix(inputPoints).map(row => row.map((cell, idx) => cell * inputMainVector[idx]))
+  const p2 = getBaseMatrix(outputPoints).map(row => row.map((cell, idx) => cell * outputMainVector[idx]))
   const transformationMatrix = math.multiply(p2, math.inv(p1))
 
   return transformationMatrix
 }
 
+const getXPlaneSquared = (x, y) => {
+  const ans1 = (
+    (- Math.pow(x, 2) * (Math.pow(x, 2) + Math.pow(y, 2) - 1) +
+      Math.sqrt(-Math.pow(x, 4) * (Math.pow(x, 2) + Math.pow(y, 2) - 1))) /
+    (Math.pow(x, 2) * (Math.pow(x, 2) + 2 * Math.pow(y, 2) - 1) + Math.pow(y, 2) * (Math.pow(y, 2) - 1))
+  )
+  if (ans1 >= 0) return ans1
+  return (
+    (- Math.pow(x, 2) * (Math.pow(x, 2) + Math.pow(y, 2) - 1) -
+      Math.sqrt(-Math.pow(x, 4) * (Math.pow(x, 2) + Math.pow(y, 2) - 1))) /
+    (Math.pow(x, 2) * (Math.pow(x, 2) + 2 * Math.pow(y, 2) - 1) + Math.pow(y, 2) * (Math.pow(y, 2) - 1))
+  )
+}
+
+const getYPlaneSquared = (x, y) => {
+  return Math.pow(y, 2) / Math.pow(x, 2) * getXPlaneSquared(x, y)
+}
+
+const revertPoint = point => {
+  const xCircle = point.x
+  const yCircle = point.y
+  const originX = (xCircle < 0 ? -1 : 1) * Math.sqrt(Math.abs(getXPlaneSquared(xCircle, yCircle)))
+  const originY = (yCircle < 0 ? -1 : 1) * Math.sqrt(Math.abs(getYPlaneSquared(xCircle, yCircle)))
+  return { x: originX, y: originY }
+}
+
+/**
+ * Convert spaces between themselves, for easier visualization it's better to use space regions between [0, 1]
+ * @param {point[]} inputPoints The input points on the normalized coordinates [-1, 1]
+ * @param {point[]} outputPoints The output points on the normalized coordinates [-1, 1]
+ * @returns {getCircleToPlaneTransformation~transformation}
+ */
+const getCircleToPlaneTransformation = (inputPoints, outputPoints) => {
+  if (inputPoints === null || outputPoints === null) {
+    return revertPoint
+  }
+
+  const outputPlanePoints = outputPoints.map(revertPoint)
+  const transformationMatrix = getTransformationMatrix(inputPoints, outputPlanePoints)
+  const inverseTransformationMatrix = math.inv(transformationMatrix)
+
+  /**
+   * 
+   * @param {point} point 
+   * @returns {point}
+   */
+  const transformation = (point) => {
+    const reversedPoint = math.multiply(inverseTransformationMatrix, [point.x, point.y, 1])
+    return {x: reversedPoint[0], y: reversedPoint[1]}
+  }
+
+  return transformation
+}
+
+const getNormalizedCoordinate = (value, size) => {
+  return 2 * value / size - 1.000001
+}
+
+const getImageCoordinates = (value, size) => {
+  return Math.round((value + 1) * size / 2)
+}
+
 export {
-  computeTransformationMatrix
+  getTransformationMatrix,
+  getCircleToPlaneTransformation,
+  getNormalizedCoordinate,
+  getImageCoordinates
 }
